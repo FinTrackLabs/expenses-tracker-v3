9739e208a69b949bc24b9cb7a30741bed02acd19
ChathurangaKumara <ChathurangaKumara@DESKTOP-IMT4SVA>
2025-01-26 19:58:56 +0530

    - Add account state - Add floating button - dashboard changes

--- src/app/views/dashboards/expense/data.ts
+++ src/app/views/dashboards/expense/data.ts
@@ -1,3 +1,5 @@
+import {VisitType} from "@views/dashboards/analytics/data";
+
 export type StateType = {
   title: string
   value: string
@@ -86,3 +88,82 @@ export const stateData: StateType[] = [
     ],
   }
 ];
+
+export const VisitsList: VisitType[] = [
+  {
+    channel: 'Organic search',
+    sessions: {
+      count: 10853,
+      percentage: 52,
+    },
+    prev_period: {
+      count: 566,
+      percentage: 92,
+    },
+    change: {
+      percentage: 52.8,
+      trend: 'up',
+    },
+  },
+  {
+    channel: 'Direct',
+    sessions: {
+      count: 2545,
+      percentage: 47,
+    },
+    prev_period: {
+      count: 498,
+      percentage: 81,
+    },
+    change: {
+      percentage: -17.2,
+      trend: 'down',
+    },
+  },
+  {
+    channel: 'Referal',
+    sessions: {
+      count: 1836,
+      percentage: 38,
+    },
+    prev_period: {
+      count: 455,
+      percentage: 74,
+    },
+    change: {
+      percentage: 41.12,
+      trend: 'up',
+    },
+  },
+  {
+    channel: 'Email',
+    sessions: {
+      count: 1958,
+      percentage: 31,
+    },
+    prev_period: {
+      count: 361,
+      percentage: 61,
+    },
+    change: {
+      percentage: -8.24,
+      trend: 'down',
+    },
+  },
+  {
+    channel: 'Social',
+    sessions: {
+      count: 1566,
+      percentage: 26,
+    },
+    prev_period: {
+      count: 299,
+      percentage: 49,
+    },
+    change: {
+      percentage: 29.33,
+      trend: 'up',
+    },
+  },
+]
+
--- src/app/views/dashboards/expense/expense.component.ts
+++ src/app/views/dashboards/expense/expense.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
 import {AccountStateComponent} from "@views/dashboards/expense/components/account-state/account-state.component";
+import {AudienceComponent} from "@views/dashboards/analytics/components/audience/audience.component";
+import {VisitorsComponent} from "@views/dashboards/analytics/components/visitors/visitors.component";
+import {RangeSelectorComponent} from "@views/dashboards/expense/components/range-selector/range-selector.component";
+import {LatestExpensesComponent} from "@views/dashboards/expense/components/latest-expenses/latest-expenses.component";
 
 @Component({
   selector: 'app-expense',
   standalone: true,
   imports: [
-    AccountStateComponent
+    AccountStateComponent,
+    AudienceComponent,
+    VisitorsComponent,
+    RangeSelectorComponent,
+    LatestExpensesComponent
   ],
   templateUrl: './expense.component.html',
   styleUrl: './expense.component.scss'
